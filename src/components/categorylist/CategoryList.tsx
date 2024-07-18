5c8f032783591ab57b1319d3bb8f6b3205b57712
abhikatta <abhinaykatta97@gmail.com>
2024-07-18 15:36:58 +0530

    feat:added category list and fixed some on featured; also made responsive upto category list

--- src/components/categorylist/CategoryList.tsx
+++ src/components/categorylist/CategoryList.tsx
@@ -1,6 +1,40 @@
+import Link from "next/link";
 import styles from "./CategoryList.module.css";
+import Image from "next/image";
 const CategoryList = () => {
-  return <div className={styles.container}>CategoryList</div>;
+  const categoryELements: {
+    name: string;
+    style: string;
+  }[] = [
+    { name: "Style", style: styles.style },
+    { name: "Fashion", style: styles.fashion },
+    { name: "Food", style: styles.food },
+    { name: "Travel", style: styles.travel },
+    { name: "Culture", style: styles.culture },
+    { name: "Coding", style: styles.coding },
+  ];
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {categoryELements.map((item, index) => (
+          <Link
+            key={index}
+            href={`/blog?cat=${item.name}`}
+            className={`${styles.category} ${item.style}`}>
+            <Image
+              src={`/${item.name.toLowerCase()}.png`}
+              alt={item.name}
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
