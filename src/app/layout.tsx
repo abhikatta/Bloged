039eb147ec4d9b696a0e4761c18b98c8651ae689
abhikatta <abhinaykatta97@gmail.com>
2024-08-06 11:11:25 +0530

    enchancement: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import { ThemeContextProvider } from "@/context/themeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <ThemeContextProvider>
           <ThemeProvider>
             <div className="container">
